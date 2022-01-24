31cc7451ae955b46f32ecee6105ef0555bc4bc16
llrs <lluis.revilla@gmail.com>
2022-01-24 18:10:25 +0100

    Add data as input (instead of in demo)

---
+++ R/input.R
@@ -0,0 +1,7 @@
+#' Input data for examples
+#'
+#' An example dataset (derived from a bit of the [ADHD200 sample](http://fcon_1000.projects.nitrc.org/indi/adhd200/))
+#' @examples
+#' data(input)
+#' dim(input)
+"input"
