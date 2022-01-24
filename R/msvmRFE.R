11ca1ca98b2bee7d2be0402f5e285890f9a81d9c
llrs <lluis.revilla@gmail.com>
2022-01-24 17:27:10 +0100

    Move around

--- R/msvmRFE.R
+++ R/msvmRFE.R
@@ -1,27 +1,26 @@
-# Copyright (C) 2011  John Colby
-# http://github.com/johncolby/SVM-RFE
-
+#' @export
 svmRFE.wrap <- function(test.fold, X, ...) {
 # Wrapper to run svmRFE function while omitting a given test fold
     train.data = X[-test.fold, ]
     test.data  = X[test.fold, ]
-    
+
     # Rank the features
     features.ranked = svmRFE(train.data, ...)
-    
+
     return(list(feature.ids=features.ranked, train.data.ids=row.names(train.data), test.data.ids=row.names(test.data)))
 }
 
+#' @export
 svmRFE <- function(X, k=1, halve.above=5000) {
 # Feature selection with Multiple SVM Recursive Feature Elimination (RFE) algorithm
     n = ncol(X) - 1
-    
+
     # Scale data up front so it doesn't have to be redone each pass
     cat('Scaling data...')
     X[, -1] = scale(X[, -1])
     cat('Done!\n')
     flush.console()
-    
+
     pb = txtProgressBar(1, n, 1, style=3)
 
     i.surviving = 1:n
@@ -31,17 +30,17 @@ svmRFE <- function(X, k=1, halve.above=5000) {
     # Recurse through all the features
     while(length(i.surviving) > 0) {
         if(k > 1) {
-            # Subsample to obtain multiple weights vectors (i.e. mSVM-RFE)            
+            # Subsample to obtain multiple weights vectors (i.e. mSVM-RFE)
             folds = rep(1:k, len=nrow(X))[sample(nrow(X))]
             folds = lapply(1:k, function(x) which(folds == x))
-            
+
             # Obtain weights for each training set
             w = lapply(folds, getWeights, X[, c(1, 1+i.surviving)])
             w = do.call(rbind, w)
-            
+
             # Normalize each weights vector
             w = t(apply(w, 1, function(x) x / sqrt(sum(x^2))))
-            
+
             # Compute ranking criteria
             v    = w * w
             vbar = apply(v, 2, mean)
@@ -58,20 +57,20 @@ svmRFE <- function(X, k=1, halve.above=5000) {
         if(length(i.surviving) == 1) {
             ranking = 1
         }
-        
+
         if(length(i.surviving) > halve.above) {
             # Cut features in half until less than halve.above
             nfeat = length(i.surviving)
             ncut  = round(nfeat / 2)
             n     = nfeat - ncut
-            
+
             cat('Features halved from', nfeat, 'to', n, '\n')
             flush.console()
-            
+
             pb = txtProgressBar(1, n, 1, style=3)
-            
+
         } else ncut = 1
-        
+
         # Update feature list
         ranked.list[i.ranked:(i.ranked-ncut+1)] = i.surviving[ranking[1:ncut]]
         i.ranked    = i.ranked - ncut
@@ -80,23 +79,25 @@ svmRFE <- function(X, k=1, halve.above=5000) {
         setTxtProgressBar(pb, n-length(i.surviving))
         flush.console()
     }
-    
+
     close(pb)
 
     return (ranked.list)
 }
 
+#' @export
 getWeights <- function(test.fold, X) {
 # Fit a linear SVM model and obtain feature weights
     train.data = X
     if(!is.null(test.fold)) train.data = X[-test.fold, ]
-    
+
     svmModel = svm(train.data[, -1], train.data[, 1], cost=10, cachesize=500,
     scale=F, type="C-classification", kernel="linear")
-    
+
     t(svmModel$coefs) %*% svmModel$SV
 }
 
+#' @export
 WriteFeatures <- function(results, input, save=T, file='features_ranked.txt') {
 # Compile feature rankings across multiple folds
     featureID = sort(apply(sapply(results, function(x) sort(x$feature, index.return=T)$ix), 1, mean), index=T)$ix
@@ -110,6 +111,7 @@ WriteFeatures <- function(results, input, save=T, file='features_ranked.txt') {
     }
 }
 
+#' @export
 FeatSweep.wrap <- function(i, results, input) {
 # Wrapper to estimate generalization error across all hold-out folds, for a given number of top features
     svm.list = lapply(results, function(x) tune(svm,
@@ -123,19 +125,22 @@ FeatSweep.wrap <- function(i, results, input) {
                                           train.y = input[x$train.data.ids, 1],
                                           ranges  = list(gamma=2^(-12:0), cost=2^(-6:6)))$best.par,
                       tunecontrol  = tune.control(sampling='fix'))$perf)
-        
+
     error = mean(sapply(svm.list, function(x) x$error))
     return(list(svm.list=svm.list, error=error))
 }
 
-PlotErrors <- function(errors, errors2=NULL, no.info=0.5, ylim=range(c(errors, errors2), na.rm=T), xlab='Number of Features',  ylab='10x CV Error') {
+#' @export
+PlotErrors <- function(errors, errors2=NULL, no.info=0.5,
+                       ylim=range(c(errors, errors2), na.rm=T),
+                       xlab='Number of Features',  ylab='10x CV Error') {
 # Makes a plot of average generalization error vs. number of top features
     AddLine <- function(x, col='black') {
         lines(which(!is.na(errors)), na.omit(x), col=col)
         points(which.min(x), min(x, na.rm=T), col='red')
         text(which.min(x), min(x, na.rm=T), paste(which.min(x), '-', format(min(x, na.rm=T), dig=3)), pos=4, col='red', cex=0.75)
     }
-    
+
     plot(errors, type='n', ylim=ylim, xlab=xlab, ylab=ylab)
     AddLine(errors)
     if(!is.null(errors2)) AddLine(errors2, 'gray30')
